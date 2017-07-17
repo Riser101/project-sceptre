bb625bb8b4f4f055cca1c8e8d3f417023c628898
yousufsyed <sy.yousuf9106@gmail.com>
2017-07-17 14:26:33 +0530

    changed port

--- src/server/server.js
+++ src/server/server.js
@@ -84,8 +84,11 @@ app.get('/*', function (req, res) {
 
 });
 
-const server = app.listen(3002, function () {
+const server = app.listen(8080, function () {
   const host = server.address().address;
   const port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
 });
+
+
+
