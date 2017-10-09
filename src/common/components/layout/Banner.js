6e21c8f7b8b5d0d6da7f8af5e8f41679735ae314
yousuf syed <sy.yousuf9106@gmail.com>
2017-10-09 19:32:32 +0530

    updated footer

--- src/common/components/layout/Banner.js
+++ src/common/components/layout/Banner.js
@@ -6,7 +6,7 @@ class Banner extends Component {
 
     return (
 	    <div className="contact-banner">
-      		<p> Please avoid unsolicited phone calls. I can be best reached at sy.yousuf9106@gmail.com </p>
+      		<p> I can be best reached at sy.yousuf9106@gmail.com </p>
       		
       	</div>
     );
