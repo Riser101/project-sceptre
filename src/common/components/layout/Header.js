3ba31348879788893476dae75713bf8582451ead
yousuf syed <sy.yousuf9106@gmail.com>
2017-12-27 18:03:09 +0530

    connected wordpress

--- src/common/components/layout/Header.js
+++ src/common/components/layout/Header.js
@@ -9,7 +9,10 @@ class Header extends Component {
 			  <h3 className="masthead-title">
 			    <a href="/" title="Home">Yousuf Syed</a>
 			    <small>Software Engineer based in Pune, India. </small>
-			    <a className="resume" href="https://drive.google.com/file/d/12ZZ8ee3eA8rQMlkJBw1SblLs6M0KGlCi/view?usp=sharing" >RESUME</a>
+			    <a target="_blank" className="resume" href="https://drive.google.com/file/d/12ZZ8ee3eA8rQMlkJBw1SblLs6M0KGlCi/view?usp=sharing" >
+			    	Resume 
+			    </a>
+			    <a href="https://iyousufsyed.wordpress.com/" className="blog"> Blog</a>
 			  </h3>
 			</div>
 	</div>
