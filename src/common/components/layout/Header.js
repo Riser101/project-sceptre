3733dfdb76372f6a016637cdeeafc15a04c50925
yousuf syed <sy.yousuf9106@gmail.com>
2018-01-12 13:48:56 +0530

    updated resume

--- src/common/components/layout/Header.js
+++ src/common/components/layout/Header.js
@@ -9,7 +9,7 @@ class Header extends Component {
 			  <h3 className="masthead-title">
 			    <a href="/" title="Home">Yousuf Syed</a>
 			    <small>Software Engineer based in Pune, India. </small>
-			    <a className="resume" href="https://drive.google.com/file/d/12ZZ8ee3eA8rQMlkJBw1SblLs6M0KGlCi/view?usp=sharing" >
+			    <a className="resume" href="https://docs.google.com/document/d/1JssJ1gMoRcrz-y2s64hk2O0O8Va72tqa5zzQUzNYi0k/edit?usp=sharing" >
 			    	Resume 
 			    </a>
 			    <a className="blog" href="https://iyousufsyed.wordpress.com/">Blog</a>
