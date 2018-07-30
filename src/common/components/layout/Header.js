f7f872eae2eb66f00e588165769cbd9d2dc94308
yousuf syed <sy.yousuf9106@gmail.com>
2018-07-30 19:29:35 +0530

    build update

--- src/common/components/layout/Header.js
+++ src/common/components/layout/Header.js
@@ -9,7 +9,7 @@ class Header extends Component {
 			  <h3 className="masthead-title">
 			    <a href="/" title="Home">Yousuf Syed</a>
 			    <small>Software Engineer based in Pune, India. </small>
-			    <a className="resume" href="https://drive.google.com/file/d/1e7Rq5uOS5t4hKjfGagT_fswhT6Q1KJhj/view" >
+			    <a className="resume" href="https://www.dropbox.com/s/49ik6ro6qury4ni/resume_online_copy.pdf?dl=0" >
 			    	Resume 
 			    </a>
 			    <a className="blog" href="https://iyousufsyed.wordpress.com/">Blog</a>
